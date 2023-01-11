264fafe6495e5b20d56d90311c05f25cd4ee6d48
Ben Mitchell <bjosephmitchell@gmail.com>
2023-01-11 11:41:44 +0000

    Floating division on controls.cpp

--- common/controls.cpp
+++ common/controls.cpp
@@ -45,11 +45,11 @@ void computeMatricesFromInputs() {
   glfwGetCursorPos(window, &xpos, &ypos);
 
   // Reset mouse position for next frame
-  glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+  glfwSetCursorPos(window, 1024 / 2., 768 / 2.);
 
   // Compute new orientation
-  horizontalAngle += mouseSpeed * float(1024 / 2 - xpos);
-  verticalAngle += mouseSpeed * float(768 / 2 - ypos);
+  horizontalAngle += mouseSpeed * float(1024 / 2. - xpos);
+  verticalAngle += mouseSpeed * float(768 / 2. - ypos);
 
   // Direction : Spherical coordinates to Cartesian coordinates conversion
   glm::vec3 direction(cos(verticalAngle) * sin(horizontalAngle),
